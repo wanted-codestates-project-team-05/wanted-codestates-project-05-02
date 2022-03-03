12db30a646f988e8ce1ca2b2e07a4621b060f52a
SeongHyun <70502670+Lee-Seonghyun316@users.noreply.github.com>
2022-03-03 22:51:27 +0900

    [Client] / #3 #4 #5 / edit: redux, firebase (#6)
    * Feat: redux state management
    
    * Feat: matchListReducer guide component 추가
    
    * Feat: proxy 설정, CORS 에러 해결
    
    * Feat: 네트워크 요청 성공 시 상태 변경 기능 추가
    
    * Feat: firebase firestore data write read function add
    
    * Feat: matchList 리듀서 액션 추가 및 네이밍 수정
    
    * Feat: matchDetail 리듀서 추가 및 사용
    
    * Fix: api key .env 처리

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './Reducer';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
