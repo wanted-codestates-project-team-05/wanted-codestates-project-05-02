2d028c386357b54f5052ee6fb2e5084c03389845
kyu <2066apple@naver.com>
2022-03-03 23:07:04 +0900

    Feat: 매치 데이터 가공

--- src/pages/TrackPage.js
+++ src/pages/TrackPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import { RankData } from '../components/RankData';
 
 function TrackPage() {
 	
 	return (
 		<div>
-			
 		</div>
 	)
 }
