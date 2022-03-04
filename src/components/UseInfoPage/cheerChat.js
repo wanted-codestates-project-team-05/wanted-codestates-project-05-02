664b45a39e93061883f5e3a36b91fbe48186f31b
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-04 13:16:49 +0900

    Feat: handle submit function add, firestroe write read function 추가

--- src/components/UseInfoPage/cheerChat.js
+++ src/components/UseInfoPage/cheerChat.js
@@ -1,29 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CardContainer from '../common/CardContainer';
+import { queryForDocument, writeCheering } from '../../service/EditFirebaseDB';
 
 const CheerChat = () => {
+  const [cheers, setCheers] = useState([]);
+  const [inputs, setInputs] = useState({
+    nickname: '',
+    oneWord: '',
+  });
+  const { nickname, oneWord } = inputs;
+  const getData = async () => {
+    const Cheers = [];
+    await Promise.all(await queryForDocument()).then((res) => {
+      res.map((item) =>
+        Cheers.push(
+          <SpeechContainer key={item.id}>
+            <Nickname>{item.nickname}</Nickname>
+            {item.data === Today ? <MarkToday /> : null}
+            <SpeechBubble>{item.oneWord}</SpeechBubble>
+          </SpeechContainer>
+        )
+      );
+    });
+    setCheers(Cheers);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const getToday = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = ('0' + (1 + date.getMonth())).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+
+    return year + '-' + month + '-' + day;
+  };
+  const Today = getToday();
+
+  const AddCheer = async (nickname, oneWord) => {
+    const cheeringId = Date.now();
+    const docData = {
+      data: Today,
+      oneWord: oneWord,
+      nickname: nickname,
+    };
+    await writeCheering(cheeringId, docData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (oneWord !== '') {
+      await AddCheer(nickname, oneWord);
+      await getData();
+    } else return;
+  };
+
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value, // <- 변경 후
+    });
+  };
+
   return (
     <CardContainer firstTitle="응원" secondTitle="한마디" des="오늘 6개 전체 13개">
-      <Content>
-        <SpeechContainer>
-          <NickName>234</NickName>
-          <MarkToday />
-          <SpeechBubble>3434</SpeechBubble>
-        </SpeechContainer>
-      </Content>
-      <Form>
-        <NickNameInput placeholder="닉네임" />
-        <WordInput placeholder="최대 30자" />
+      <Content>{cheers.length !== 0 ? cheers : null}</Content>
+      <Form onSubmit={handleSubmit}>
+        <NicknameInput placeholder="닉네임" value={nickname} name="nickname" onChange={handleChange} />
+        <WordInput placeholder="최대 30자" value={oneWord} name="oneWord" onChange={handleChange} />
         <Button>남기기</Button>
       </Form>
     </CardContainer>
   );
 };
-
 export default CheerChat;
 
 const Content = styled.div`
+  overflow-y: scroll;
   display: flex;
   flex-direction: column;
   height: 159px;
@@ -32,17 +86,16 @@ const Content = styled.div`
 `;
 
 const SpeechContainer = styled.div`
+  margin: 6px 0;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-  overflow-x: hidden;
-  overflow-y: auto;
   font-size: 12px;
   font-weight: 600;
 `;
 
-const NickName = styled.span`
+const Nickname = styled.span`
   color: ${({ theme }) => theme.color.blue};
 `;
 
@@ -54,6 +107,7 @@ const MarkToday = styled.div`
 `;
 
 const SpeechBubble = styled.div`
+  margin-left: 15px;
   position: relative;
   width: 210px;
   border: 1px solid #c3ced5;
@@ -91,8 +145,9 @@ const Form = styled.form`
   margin: 10px 12px;
 `;
 
-const NickNameInput = styled.input`
-  padding-bottom: 5px;
+const NicknameInput = styled.input`
+  height: 16px;
+  padding: 7px 5px;
   margin-right: 5px;
   vertical-align: middle;
   width: 15%;
@@ -102,16 +157,18 @@ const NickNameInput = styled.input`
 `;
 
 const WordInput = styled.input`
-  padding-bottom: 5px;
+  height: 16px;
+  padding: 7px 5px;
   margin-right: 5px;
   vertical-align: middle;
-  width: 60%;
+  width: 55%;
   border: none;
   border-bottom: 1px solid ${({ theme }) => theme.color.conTour};
   font-size: 12px;
 `;
 
 const Button = styled.button`
+  cursor: pointer;
   border: 1px solid ${({ theme }) => theme.color.conTour};
   background-color: ${({ theme }) => theme.color.conTour};
   color: ${({ theme }) => theme.color.white};
