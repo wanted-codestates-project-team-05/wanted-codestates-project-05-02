7b1c37a9a13e0d83e5a1351dbfcb81824a636484
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-05 00:11:43 +0900

    Feat: track, card ui 조합

--- src/pages/UserInfoPage.js
+++ src/pages/UserInfoPage.js
@@ -8,6 +8,7 @@ import HomePage from './HomePage';
 import { fetchUserMatchList } from '../Reducer/matchList';
 import { useDispatch, useSelector } from 'react-redux';
 import Loading from '../components/common/Loading';
+import InfoContainer from '../components/UserInfoPage/InfoContainer';
 
 function UserInfoPage() {
   //리덕스
@@ -47,7 +48,7 @@ function UserInfoPage() {
         <Content>
           <UserProfile char={matchList.userMatchList.data?.matches[0].matches[0].character} />
           <HomePage userMatchList={matchList.userMatchList.data?.matches[0].matches} />
-          {/*<InfoContainer userData={user.data} matchData={matchList.userMatchList.data?.matches[0].matches} />*/}
+          <InfoContainer />
         </Content>
       )}
     </Wrap>
