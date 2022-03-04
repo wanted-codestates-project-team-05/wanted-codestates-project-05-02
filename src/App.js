f25e7a2a5a8023690d3c0823226ce573d5056ba8
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-04 23:57:31 +0900

    Fix: 충돌 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './reset.css';
-import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import React from 'react';
+import Routes from './Routes';
+import UserInfoPage from './pages/UserInfoPage';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div>
         <Routes />
-        <UserInfoPage />
       </div>
     </ThemeProvider>
   );
