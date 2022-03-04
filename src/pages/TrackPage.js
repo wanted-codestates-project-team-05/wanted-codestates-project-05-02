08503a7aeac5691bb14c7e9a8573150f6aaffdb5
kyu <2066apple@naver.com>
2022-03-04 10:18:22 +0900

    Feat: 스피드 개인전 데이터 가공

--- src/pages/TrackPage.js
+++ src/pages/TrackPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { RankData } from '../components/RankData';
 
 function TrackPage() {
 	
 	return (
 		<div>
-			<RankData/>
+			
 		</div>
 	)
 }
